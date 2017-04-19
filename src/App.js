b7e64e38469ebbfa635cb9f413f3995ec74b39a7
stkraft <steffen.kraft@mail.de>
2017-04-19 17:16:10 +0200

    overwrite muiTheme and adding contact form with handling form data

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 
 import Layout from './layout'
 import About from './routes/about'
 import Home from './routes/home'
 import Contact from './routes/contact'
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#eda821'
+  },
+});
+
+
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider muiTheme={muiTheme}>
         <Router>
           <Layout>
             <Route exact path="/" component={Home} />
--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -1,15 +1,97 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
+import Toggle from 'material-ui/Toggle'
+import Checkbox from 'material-ui/Checkbox'
 
-const Contact = () => (
-    <div>
-        <Helmet>
-            <title>Contact - My Reactplay Ground</title>
-            <meta name="description" content="My Reactplay Ground - Contactpage" />
-        </Helmet>
-        <h1>Contact</h1>
-        <p>welcome to the contact page</p>
-    </div>
-)
+class Contact extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            firstname: '',
+            name: '',
+            street: '',
+            newsletter: false,
+            email: '',
+            billing: false
+        }
+    }
+
+    handleInputChange = (event) => {
+        const target = event.target
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        this.setState({
+            [target.name]: value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        // TODO handle fom data from this.state
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <div>
+                <Helmet>
+                    <title>Contact - My Reactplay Ground</title>
+                    <meta name="description" content="My Reactplay Ground - Contactpage" />
+                </Helmet>
+                <h1>Contact</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <TextField
+                        onChange={this.handleInputChange}
+                        hintText="Please enter your Firstname"
+                        floatingLabelText="Firstname"
+                        fullWidth={true}
+                        name="firstname"
+                    />
+                    <TextField
+                        onChange={this.handleInputChange}
+                        hintText="Please enter your Name"
+                        floatingLabelText="Name"
+                        fullWidth={true}
+                        name="name"
+                        // errorText="Please put in your Name"
+                    />
+                    <TextField
+                        onChange={this.handleInputChange}
+                        hintText="Please enter your Street"
+                        floatingLabelText="Street"
+                        fullWidth={true}
+                        name="street"
+                    />
+                    <TextField
+                        onChange={this.handleInputChange}
+                        hintText="Please enter your Email"
+                        floatingLabelText="Email"
+                        fullWidth={true}
+                        name="email"
+                    />
+                    <Checkbox
+                        onCheck={this.handleInputChange}
+                        label="Newsletter"
+                        name="newsletter"
+                    />
+                    <Toggle
+                        onToggle={this.handleInputChange}
+                        label="Label on the right"
+                        name="billing"
+                        labelStyle={{textAlign: 'right'}}
+                    />
+                    <RaisedButton
+                        label="Primary"
+                        primary={true}
+                        fullWidth={true}
+                        type="submit"
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+
 
 export default Contact
