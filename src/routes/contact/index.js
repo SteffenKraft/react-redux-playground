b1bd6b8c956e2b0790ed687cd1d41d0fff35f772
stkraft <steffen.kraft@mail.de>
2017-04-19 10:16:41 +0200

    adding Helmet

--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 const Contact = () => (
     <div>
+        <Helmet>
+            <title>Contact - My Reactplay Ground</title>
+            <meta name="description" content="My Reactplay Ground - Contactpage" />
+        </Helmet>
         <h1>Contact</h1>
         <p>welcome to the contact page</p>
     </div>
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 const Home = () => (
     <div>
+        <Helmet>
+            <title>Home - My Reactplay Ground</title>
+            <meta name="description" content="My Reactplay Ground - Homepage" />
+        </Helmet>
         <h1>Home</h1>
         <p>welcome to the react playground</p>
     </div>
